9cbdbef7c20abe49f293a8f4ac779f6b0cfa3d72
Денислав Ангелов <denyslavangelov@abv.bg>
2025-09-05 11:20:26 +0300

    save

--- src/cdn/shopify-integration.js
+++ src/cdn/shopify-integration.js
@@ -37,6 +37,7 @@
         box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
         z-index: 10000;
         display: none;
+        margin: 0 auto;
       "
       allow="clipboard-write"
     ></iframe>
