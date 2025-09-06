4b32fa0abdb600db506216c1595e8aa33c2d0011
Денислав Ангелов <denyslavangelov@abv.bg>
2025-09-06 18:04:56 +0300

    save

--- src/cdn/shopify-integration.js
+++ src/cdn/shopify-integration.js
@@ -61,8 +61,8 @@
         height: auto;
         min-height: 600px;
         max-height: 90vh;
-        border-radius: 10px;
-        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        // border-radius: 10px;
+        // box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
         z-index: 10000;
         display: none;
         background-color: transparent;
